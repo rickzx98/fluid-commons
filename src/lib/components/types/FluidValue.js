2a026bbdaac21459b5133ffc0567307b5d1f0d44
Jerico <rickzx98@gmail.com>
2018-03-30 17:13:22 +0800

    added getPrimaryField

--- src/lib/components/types/FluidValue.js
+++ src/lib/components/types/FluidValue.js
@@ -1,6 +1,7 @@
 export default class FormValue {
     constructor(fieldDefinitions, value) {
-        this.getPrimaryKey = () =>  getPrimaryKey(fieldDefinitions, value);
+        this.getPrimaryKey = () => getPrimaryKey(fieldDefinitions, value);
+        this.getPrimaryField = () => getPrimaryField(fieldDefinitions);
         for (let field in value) {
             if (value.hasOwnProperty(field)) {
                 this[field] = value[field];
@@ -13,4 +14,10 @@ function getPrimaryKey(fieldDefinitions, value) {
     return fieldDefinitions
         .filter(definition => definition.primaryKey)
         .map(definition => value[definition.field])[0];
+}
+
+function getPrimaryField(fieldDefinitions) {
+    return fieldDefinitions
+        .filter(definition => definition.primaryKey)
+        .map(definition => definition.field)[0];
 }
